440fc99fcf5ea74fe4f0818b1558e222999845dd
Ohia George <ohiageorge@gmail.com>
2022-09-11 10:20:35 +0000

    [FIX] CI/CD

--- redeploy.sh
+++ redeploy.sh
@@ -1,4 +1,13 @@
-#!bin/bash
-docker stop etdp-qa
-docker system prune -f
-docker compose up -d --force-recreate
+#!/usr/bin/bash
+echo "Building Docker image"
+#sudo docker stop watchtower
+sudo docker stop
+#sudo docker system prune -f
+echo "Removing old images"
+sudo docker-compose rm -f
+echo "Downloading new images"
+sudo docker-compose pull
+echo "Staring the container"
+sudo docker-compose  up -d
+echo "Removing any dangling image"
+sudo docker image prune -f
